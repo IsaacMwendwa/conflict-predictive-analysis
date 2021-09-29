60f5a136f49f0ee3b67673b63b5cfa32631e4c55
Isaac Mwendwa <isaacmwendwa107@gmail.com>
2021-09-29 23:46:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -129,6 +129,6 @@ def result():
     
 
 if __name__ == "__main__":
-    #port = int(os.environ.get("PORT", 5000))
-    #app.run(host='0.0.0.0', port=port)
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    #app.run()
